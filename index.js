e552c691cc868276fd1421be7498a6f42fac164e
digitechmahmud <mdmahmudulhassanbd@gmail.com>
2022-11-09 11:51:41 +0600

    Post service data to Mongodb

--- index.js
+++ index.js
@@ -62,7 +62,12 @@ async function run() {
             res.send(review);
         })
         
-        
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
+
         app.post('/reviews', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
